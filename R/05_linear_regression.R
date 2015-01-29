13ccc23feb0484b63b041a9ab9fc68f2f9e627a9
Andrew Ellis <a.w.ellis@gmail.com>
2015-01-29 08:18:15 +0100

    added logistic regression exercise

--- R/05_linear_regression.R
+++ R/05_linear_regression.R
@@ -1,12 +1,12 @@
 
 N <- 1000 # number of observations
 
-x <- 1:N # predictor variable
+x <- seq(from = -2, to = 2, length.out = N) # predictor variable
 
-a <- 5 # intercept
-b <- 1.5 # slope
+a <- -1 # intercept
+b <- 0.8 # slope
 
-sigma <- 5 #sampling noise
+sigma <- 1.2 #sampling noise
 
 y_hat <- a + b*x # linear predictor
 
@@ -49,6 +49,57 @@ fit <- jags(data = jagsdata, model.file = textConnection(modelstr),
              inits = inits, n.chains = 2,
              n.iter = 2000, n.thin = 2,n.burnin = 100,
              parameters.to.save = parameters)
+
+print(fit)
+
+library(mcmcplots)
+caterplot(fit, parms = c("a", "b", "sigma"))
+
+set.seed(3425)
+
+N <- 1000 # number of observations
+
+x <- seq(from = -2, to = 2, length.out = N) # predictor variable
+
+a <- -1 # intercept
+b <- 0.8 # slope
+
+z_hat <- plogis(a + b*x) # linear predictor on the logit scale
+
+z <- rbinom(N, prob = z_hat, size = 1)  # response variable
+
+library(ggplot2)
+qplot(x, z, geom = "point")
+
+modelstr <- "
+model {
+  for (i in 1:N){
+    z[i] ~ dbern(z_hat[i])
+    logit(z_hat[i]) <- a + b * x[i]
+}
+
+  # priors on the coefficients
+  a ~ dnorm(0, 1e-03)
+  b ~ dnorm(0, 1e-03)
+}"
+
+library(R2jags)
+
+jagsdata <- list(z = z,
+                 x = x,
+                 N = length(z))
+
+inits <- function() {
+    list(a = rnorm(1, 0, 10),
+         b = rnorm(1, 0, 10))
+}
+
+parameters = c("a", "b")
+
+fit <- jags(data = jagsdata, model.file = textConnection(modelstr),
+            inits = inits, n.chains = 2,
+            n.iter = 2000, n.thin = 2,n.burnin = 100,
+            parameters.to.save = parameters)
 
 print(fit)
 
